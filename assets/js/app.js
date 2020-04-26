d8cdaa9929e5731d5adf0745bc2fb170bd378974
Salvador <salvador.on@outlook.com>
2020-04-26 19:42:07 -0300

    draw board

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,29 @@ class TicTacToeGame {
   start() {
     this.board = this.board();
     this.currentPlayer = this.playerX;
+    this.drawBoard();
+  }
+
+  drawBoard() {
+    document.getElementById("cont").innerHTML = "";
+    document.getElementById("form").innerHTML = "";
+    let gameBoard = document.createElement('div');
+    gameBoard.id = 'gameBoard';
+    gameBoard.classList.add('board', 'd-flex', 'bg-warning', 'flex-wrap');
+    gameBoard.addEventListener('click', this.handleSquareClick.bind(this))
+
+    this.board.board.forEach((squre, index) => {
+      let sqr = document.createElement('div');
+      sqr.id = index;
+      sqr.classList.add('sqr');
+      gameBoard.appendChild(sqr);
+    });
+    document.getElementById("cont").appendChild(gameBoard);
   }
 
 
+  
+ 
 
 
 
