c364dd2f2ada6d478943e9417173e5ac45029825
Salvador <salvador.on@outlook.com>
2020-04-27 11:43:09 -0300

    change board object

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,25 +6,25 @@ class TicTacToeGame {
 
   board = () => {
     return {
-      board: ["", "", "",
+      arrayboard: ["", "", "",
         "", "", "",
         "", "", ""]
     }
   }
 
-  set() {
+  set = () => {
     this.playerX = this.player(document.getElementById('nameUserX').value, "X");
     this.playerO = this.player(document.getElementById('nameUserO').value, "O");
     this.start();
   }
 
-  start() {
+  start = () => {
     this.board = this.board();
     this.currentPlayer = this.playerX;
     this.drawBoard();
   }
 
-  drawBoard() {
+  drawBoard = () => {
     document.getElementById("cont").innerHTML = "";
     document.getElementById("form").innerHTML = "";
     let gameBoard = document.createElement('div');
@@ -32,7 +32,7 @@ class TicTacToeGame {
     gameBoard.classList.add('board', 'd-flex', 'bg-warning', 'flex-wrap');
     gameBoard.addEventListener('click', this.squareClick.bind(this))
 
-    this.board.board.forEach((box, index) => {
+    this.board.arrayboard.forEach((box, index) => {
       let sqr = document.createElement('div');
       sqr.id = index;
       sqr.classList.add('sqr');
@@ -41,26 +41,26 @@ class TicTacToeGame {
     document.getElementById("cont").appendChild(gameBoard);
   }
 
-  squareClick(event) {
+  squareClick = (event) => {
     this.executeMove(event.target.id);
 
   }
 
-  executeMove(moveIndex) {
-    if (this.board.board[moveIndex] == "") {
-      this.board.board[moveIndex] = this.currentPlayer.marker;
+  executeMove = (moveIndex) => {
+    if (this.board.arrayboard[moveIndex] == "") {
+      this.board.arrayboard[moveIndex] = this.currentPlayer.marker;
       this.updateBoard();
       this.currentPlayer = (this.currentPlayer == this.playerX ? this.playerO : this.playerX);
     }
   }
 
-  updateBoard() {
-    console.log(this.board.board)
+  updateBoard = () => {
+    console.log(this.board.arrayboard)
     let gameBoard = document.getElementById('gameBoard');
     let sqr = gameBoard.childNodes;
     sqr.forEach((element, index) => {
-      if (element.innerText != this.board.board[index]) {
-        element.innerText = this.board.board[index];
+      if (element.innerText != this.board.arrayboard[index]) {
+        element.innerText = this.board.arrayboard[index];
       }
     })
   }
