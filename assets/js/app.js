02a36f070ed6fcea396e6bd95590916cf48af348
Salvador <salvador.on@outlook.com>
2020-04-27 14:21:25 -0300

    add wining logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,111 @@
 class TicTacToeGame {
-
-  player = (name, marker) => {
+  player(name, marker) {
     return { name, marker };
   }
 
-  board = () => {
+  board() {
     return {
-      arrayboard: ["", "", "",
-        "", "", "",
-        "", "", ""]
-    }
+      arrayboard: ['', '', '',
+        '', '', '',
+        '', '', ''],
+    };
   }
 
-  set = () => {
-    this.playerX = this.player(document.getElementById('nameUserX').value, "X");
-    this.playerO = this.player(document.getElementById('nameUserO').value, "O");
+  set() {
+    this.playerX = this.player(document.getElementById('nameUserX').value, 'X');
+    this.playerO = this.player(document.getElementById('nameUserO').value, 'O');
     this.start();
   }
 
-  start = () => {
+  start() {
     this.board = this.board();
     this.currentPlayer = this.playerX;
     this.drawBoard();
   }
 
-  drawBoard = () => {
-    document.getElementById("cont").innerHTML = "";
-    document.getElementById("form").innerHTML = "";
-    let gameBoard = document.createElement('div');
+  drawBoard() {
+    document.getElementById('cont').innerHTML = '';
+    document.getElementById('form').innerHTML = '';
+    const gameBoard = document.createElement('div');
     gameBoard.id = 'gameBoard';
     gameBoard.classList.add('board', 'd-flex', 'bg-warning', 'flex-wrap');
-    gameBoard.addEventListener('click', this.squareClick.bind(this))
+    gameBoard.addEventListener('click', this.squareClick.bind(this));
 
     this.board.arrayboard.forEach((box, index) => {
-      let sqr = document.createElement('div');
+      const sqr = document.createElement('div');
       sqr.id = index;
       sqr.classList.add('sqr');
       gameBoard.appendChild(sqr);
     });
-    document.getElementById("cont").appendChild(gameBoard);
+    document.getElementById('cont').appendChild(gameBoard);
   }
 
-  squareClick = (event) => {
+  squareClick(event) {
     this.executeMove(event.target.id);
-
   }
 
-  executeMove = (moveIndex) => {
-    if (this.board.arrayboard[moveIndex] == "") {
-      this.board.arrayboard[moveIndex] = this.currentPlayer.marker;
+  executeMove(movement) {
+    if (this.board.arrayboard[movement] === '' && !this.gameHasWinner()) {
+      this.board.arrayboard[movement] = this.currentPlayer.marker;
       this.updateBoard();
-      this.currentPlayer = (this.currentPlayer == this.playerX ? this.playerO : this.playerX);
+      if (!this.gameHasWinner()) {
+        if (this.gamenoWinner()) {
+          this.displaytie();
+        }
+        this.currentPlayer = (this.currentPlayer === this.playerX ? this.playerO : this.playerX);
+      } else {
+        this.displayWinner();
+      }
     }
   }
 
-  updateBoard = () => {
-    console.log(this.board.arrayboard)
-    let gameBoard = document.getElementById('gameBoard');
-    let sqr = gameBoard.childNodes;
+  updateBoard() {
+    const gameBoard = document.getElementById('gameBoard');
+    const sqr = gameBoard.childNodes;
     sqr.forEach((element, index) => {
-      if (element.innerText != this.board.arrayboard[index]) {
+      if (element.innerText !== this.board.arrayboard[index]) {
         element.innerText = this.board.arrayboard[index];
       }
-    })
+    });
+  }
+
+  gameHasWinner() {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6],
+    ];
+
+    return winningCombos.find((combo) => {
+      if (this.board.arrayboard[combo[0]] !== '' && this.board.arrayboard[combo[1]] !== '' && this.board.arrayboard[combo[2]] !== '' && this.board.arrayboard[combo[0]] === this.board.arrayboard[combo[1]] && this.board.arrayboard[combo[1]] === this.board.arrayboard[combo[2]]) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  gamenoWinner() {
+    for (let i = 0; i < this.board.arrayboard.length; i += 1) {
+      if (this.board.arrayboard[i] === '') return false;
+    }
+    return true;
+  }
+
+  displayWinner() {
+    const message = document.createElement('div');
+    message.id = 'message';
+    message.classList.add('text-center', 'text-warning', 'font-weight-bold', 'h2', 'pt-4');
+    message.innerHTML = ` Congratulations ${this.currentPlayer.name} Win`;
+    document.getElementById('cont').appendChild(message);
+  }
+
+  displaytie() {
+    const message = document.createElement('div');
+    message.id = 'message';
+    message.classList.add('text-center', 'text-warning', 'font-weight-bold', 'h2', 'pt-4');
+    message.innerHTML = `${this.playerX.name} and ${this.playerO.name} it's a tie, play agian`;
+    document.getElementById('cont').appendChild(message);
   }
 }
 
-
-game = new TicTacToeGame();
+const game = new TicTacToeGame();
